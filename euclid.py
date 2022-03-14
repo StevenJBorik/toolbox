d85633422bb9cff73962bee2d0d9ebe6f473dedc
Steven Borblob <stevenjborik@protonmail.com>
2022-03-14 13:53:05 -0500

    euclid algo

---
+++ euclid.py
@@ -0,0 +1,19 @@
+# recursive function for calculating greatest common divisor of two numbers of two numbers
+# a = bq + r
+# gcd(a, b) = gcd (b, r)
+
+def gcd(a, b): 
+    if b == 0: 
+        return a
+    else: 
+       return gcd(b, a % b)
+
+print(gcd(102, 92319))
+
+
+def lcm(a, b):
+    if a == 0 and b == 0:
+        return 0
+    return abs(a * b) // gcd(a, b)
+
+print(lcm(10, 20)) 
