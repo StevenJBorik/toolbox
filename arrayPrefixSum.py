0e323735e01973025aa5a8f0980f5a9e318b080d
Steven Borblob <stevenjborik@protonmail.com>
2022-04-15 16:18:29 -0500

    array prefix sum

---
+++ arrayPrefixSum.py
@@ -0,0 +1,15 @@
+# initializing list
+test_list = [3, 4, 1, 7, 9, 1]
+  
+# printing original list
+print("The original list : " + str(test_list))
+  
+# using list comprehension + sum() + list slicing
+# prefix sum list
+for i in range(len(test_list)):
+    print(sum(test_list [: i + 1]))
+
+res = [sum(test_list[ : i + 1]) for i in range(len(test_list))]
+   
+# print result
+print("The prefix sum list is : " + str(res))
